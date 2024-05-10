fdaf5b9ce53eca4b7251832543b4ad291e503129
Augusto Romero <augustoromerodev@gmail.com>
2024-05-09 23:51:05 -0300

    query relevant courses with closest locations

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -15,15 +15,40 @@ export async function GET(request: Request) {
     const embedding = await generateEmbedding(query)
     const vectorQuery = `[${embedding.join(',')}]`
     const courses = await prisma.$queryRaw`
-      SELECT
-      "nombre", "nivelEducativo", "url", "descripcion", 
-      1 - (embedding <=> ${vectorQuery}::vector) as similarity
-      FROM courses
-      WHERE 1 - (embedding <=> ${vectorQuery}::vector) >= .8
-      AND "nivel" <= ${Number(nivel)}
-      AND "nivel" > 0
-      ORDER BY similarity DESC
-      LIMIT 5;
+    WITH TopCourses AS (
+        SELECT
+            c.id,
+            c.nombre,
+            c.url,
+            c.descripcion,
+            1 - (c.embedding <=> ${vectorQuery}::vector) as similarity
+        FROM courses c
+        WHERE 1 - (c.embedding <=> ${vectorQuery}::vector) >= 0.8
+          AND c.nivel <= ${Number(nivel)}
+          AND c.nivel > 0
+        ORDER BY similarity DESC
+        LIMIT 5
+    )
+    SELECT
+        tc.id,
+        tc.nombre,
+        tc.url,
+        tc.descripcion,
+        tc.similarity,
+        array(
+            SELECT json_build_object(
+                'nombre', s.nombre,
+                'departamento', s.departamento,
+                'localidad', s.localidad,
+                'calle', s.calle
+            )
+            FROM schools s
+            JOIN courses c ON s.codigo = ANY(c.ubicaciones)
+            WHERE c.id = tc.id
+            ORDER BY ST_Distance(s.location, ST_SetSRID(ST_MakePoint(-34.790775, -55.921293), 4326)::geography) ASC
+            LIMIT 5
+        ) AS closest_locations
+    FROM TopCourses tc;
     `
     return NextResponse.json({ courses: courses as Array<Course & { similarity: number }> }, { status: 200 });
   } catch (error) {
